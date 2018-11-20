24569f4e87801a3f3adef6ce2325d11e230e1005
fb671687 <fb671687@LN01006>
2018-11-20 14:34:43 -0600

    Placed code into repository

---
+++ GroupProjectTaffyStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GroupProjectTaffyStore.Startup))]
+namespace GroupProjectTaffyStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
