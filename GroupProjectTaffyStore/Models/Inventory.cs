d61f7ff525eba5a2ae6aa13bd858afd8836c113f
fb671687 <fb671687@LN01006>
2018-11-29 14:03:13 -0600

    This stuff's unnecessary

---
+++ GroupProjectTaffyStore/Models/Inventory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GroupProjectTaffyStore.Models
+{
+    public class Inventory
+    {
+        public int Id { get; set; }
+        public string Flavor { get; set; }
+        public string Brand { get; set; }
+        public decimal Price { get; set; }
+        public int CurrentStock { get; set; }
+    }
+}
